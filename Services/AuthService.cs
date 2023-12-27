a71eaf325d8fa485c6fb51d5022af01c74b8e242
The D Akbari <diya0904ak@gmail.com>
2023-12-27 20:36:07 +0530

    Service

---
+++ Services/AuthService.cs
@@ -0,0 +1,117 @@
+﻿namespace WONDERLUST_PROJECT_APIs.Services
+{
+    using WONDERLUST_PROJECT_APIs.Data;
+    using WONDERLUST_PROJECT_APIs.Models.DbModels;
+    using Microsoft.IdentityModel.Tokens;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+    using WONDERLUST_PROJECT_APIs.Helpers;
+    using BC = BCrypt.Net.BCrypt;
+
+    public class AuthService
+    {
+        private readonly DataContext dataContext;
+        private readonly IConfiguration configuration;
+
+        public AuthService(DataContext dataContext, IConfiguration configuration)
+        {
+            this.dataContext = dataContext;
+            this.configuration = configuration;
+        }
+
+        public bool IsAuthenticated(string email, string password)
+        {
+            var user = this.GetByEmail(email);
+            return this.DoesUserExists(email) && BC.Verify(password, user.Password);
+        }
+
+        public bool DoesUserExists(string email)
+        {
+            var user = this.dataContext.Users.FirstOrDefault(x => x.Email == email);
+            return user != null;
+        }
+
+        public User GetById(string id)
+        {
+            return this.dataContext.Users.FirstOrDefault(c => c.UserId == id);
+        }
+
+        public User[] GetAll()
+        {
+            return this.dataContext.Users.ToArray();
+        }
+
+        public User GetByEmail(string email)
+        {
+            return this.dataContext.Users.FirstOrDefault(c => c.Email == email);
+        }
+
+        public User RegisterUser(User model)
+        {
+            var id = IdGenerator.CreateLetterId(10);
+            var existWithId = this.GetById(id);
+            while (existWithId != null)
+            {
+                id = IdGenerator.CreateLetterId(10);
+                existWithId = this.GetById(id);
+            }
+            model.UserId = id;
+            model.Password = BC.HashPassword(model.Password);
+
+            var userEntity = this.dataContext.Users.Add(model);
+            this.dataContext.SaveChanges();
+
+            return userEntity.Entity;
+        }
+
+        public string GenerateJwtToken(string email, string role)
+        {
+            var issuer = this.configuration["Jwt:Issuer"];
+            var audience = this.configuration["Jwt:Audience"];
+            var key = Encoding.ASCII.GetBytes(this.configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                            new Claim("Id", Guid.NewGuid().ToString()),
+                            new Claim(JwtRegisteredClaimNames.Sub, email),
+                            new Claim(JwtRegisteredClaimNames.Email, email),
+                            new Claim(ClaimTypes.Role, role),
+                            new Claim(JwtRegisteredClaimNames.Jti,
+                            Guid.NewGuid().ToString())
+                        }),
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public string DecodeEmailFromToken(string token)
+        {
+            var decodedToken = new JwtSecurityTokenHandler();
+            var indexOfTokenValue = 7;
+
+            var t = decodedToken.ReadJwtToken(token.Substring(indexOfTokenValue));
+
+            return t.Payload.FirstOrDefault(x => x.Key == "email").Value.ToString();
+        }
+
+        public User ChangeRole(string email, string role)
+        {
+            var user = this.GetByEmail(email);
+            user.Role = role;
+            this.dataContext.SaveChanges();
+
+
+            return user;
+        }
+
+
+       
+    }
+}
